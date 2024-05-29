bd7329c2f15806b22d951ee0e3e8767e7190d80c
nurqahar <adha.nur.qahar@gmail.com>
2024-05-29 20:07:35 +0700

    hapus FILE 1.txt & modif array.js

--- code/29-5-2024/array.js
+++ code/29-5-2024/array.js
@@ -1,1 +1,18 @@
-let num=[1,2,3,4,5];
+const myArray=['Apple', 'Banana', 'Cherry'];
+const outputDiv = document.getElementById("arrayOutput");
+
+let content='<ul>';
+// for(let i=0;i<myArray.length;i++){
+//     content = content + `<li>${myArray[i]}</li>`;
+//     console.log(content);   
+// }
+
+// for loop JS style
+for(let item of myArray){
+    content = content + `<li>${item}</li>`;
+    console.log(content);   
+}
+console.log(content);
+content+='</ul>';
+
+outputDiv.innerHTML=content;
