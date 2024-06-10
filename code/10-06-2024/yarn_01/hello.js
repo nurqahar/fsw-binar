e5110fa01e9faf3e69a275b65d9e97c3b466c8a2
nurqahar <adha.nur.qahar@gmail.com>
2024-06-10 22:07:14 +0700

    modified files

--- code/10-06-2024/yarn_01/hello.js
+++ code/10-06-2024/yarn_01/hello.js
@@ -1,8 +1,8 @@
 let colors = require("colors");
 let hitungLuasSegitiga = require("./math");
-// let hitungLuasPersegiPanjang = require("./math");
+let hitungLuasPersegiPanjang = require("./math");
 console.log("hello".bgCyan.red);
 console.log("world".bgWhite.green);
 
 console.log(hitungLuasSegitiga(5, 5));
-// console.log(hitungLuasPersegiPanjang(2 * 2));
+console.log(hitungLuasPersegiPanjang(2, 2));
--- code/10-06-2024/yarn_01/math.js
+++ code/10-06-2024/yarn_01/math.js
@@ -2,9 +2,9 @@ function hitungLuasSegitiga(alas, tinggi) {
   return (alas * tinggi) / 2;
 }
 
-// function hitungLuasPersegiPanjang(panjang, lebar) {
-//   return panjang * lebar;
-// }
+function hitungLuasPersegiPanjang(panjang, lebar) {
+  return panjang * lebar;
+}
 
 module.exports = hitungLuasSegitiga;
-// module.exports = hitungLuasPersegiPanjang;
+module.exports = hitungLuasPersegiPanjang;
