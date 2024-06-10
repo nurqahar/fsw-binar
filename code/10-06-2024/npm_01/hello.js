657b1b77205e0925369f37a278f3115033183e76
nurqahar <adha.nur.qahar@gmail.com>
2024-06-10 21:14:41 +0700

    add yarn, move npm file

---
+++ code/10-06-2024/npm_01/hello.js
@@ -0,0 +1,5 @@
+let colors = require("colors");
+
+console.log(colors.red("hello"));
+console.log(colors.bgGreen("world"));
+console.log("HELLO 1".bgYellow.black.bold);
---
+++ code/10-06-2024/yarn_01/hello.js
@@ -0,0 +1,3 @@
+let colors = require("colors");
+console.log("hello".bgCyan.red);
+console.log("world".bgWhite.green);
