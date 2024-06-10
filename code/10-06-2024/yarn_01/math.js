7c1b6b164fbb19198aba4fbace7bd1b5f85375c0
nurqahar <adha.nur.qahar@gmail.com>
2024-06-10 22:04:16 +0700

    add file math.js

---
+++ code/10-06-2024/yarn_01/math.js
@@ -0,0 +1,10 @@
+function hitungLuasSegitiga(alas, tinggi) {
+  return (alas * tinggi) / 2;
+}
+
+// function hitungLuasPersegiPanjang(panjang, lebar) {
+//   return panjang * lebar;
+// }
+
+module.exports = hitungLuasSegitiga;
+// module.exports = hitungLuasPersegiPanjang;
